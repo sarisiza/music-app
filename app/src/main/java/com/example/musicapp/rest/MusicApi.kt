728293ece20c5edb153d924328a84d4e937a4beb
Sara Iza <sasita.iza@gmail.com>
2023-02-06 21:16:21 -0500

    Create MusicApi.kt interface

--- app/src/main/java/com/example/musicapp/rest/MusicApi.kt
+++ app/src/main/java/com/example/musicapp/rest/MusicApi.kt
@@ -1,21 +1,19 @@
 package com.example.musicapp.rest
 
 import com.example.musicapp.model.MusicResponse
-import com.example.musicapp.model.SongItem
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MusicApi {
 
-    @GET()
+    @GET("search")
     suspend fun getSongs(
         @Query("term") genre: String
-    ): Response<MusicResponse>
+    ): Response<List<MusicResponse>>
 
-    //https://itunes.apple.com/search?term=rock
     companion object{
-        const val BASE_URL = "https://itunes.apple.com/search"
+        const val BASE_URL = "https://itunes.apple.com/"
     }
 
 }
