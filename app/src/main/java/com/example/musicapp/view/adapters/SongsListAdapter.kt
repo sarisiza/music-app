3d766305119da52aa55447576e91a13dcffe27ad
Sara Iza <sasita.iza@gmail.com>
2023-02-07 23:34:25 -0500

    Created SongsListAdapter.kt class added Picasso to build.gradle

---
+++ app/src/main/java/com/example/musicapp/view/adapters/SongsListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.musicapp.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.musicapp.databinding.SongViewBinding
+import com.example.musicapp.model.SongItem
+import com.squareup.picasso.Picasso
+
+class SongsListAdapter(
+    private val songsList: MutableList<SongItem> = mutableListOf(),
+    private val onClickedSong: (Int) -> Unit
+): RecyclerView.Adapter<SongsListViewHolder>() {
+
+    fun updateSongs(newSongs: List<SongItem>){
+        if(songsList != newSongs){
+            songsList.addAll(newSongs)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongsListViewHolder {
+        return SongsListViewHolder(
+            SongViewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = songsList.size
+
+    override fun onBindViewHolder(holder: SongsListViewHolder, position: Int) {
+        holder.songsBinding(songsList[position],onClickedSong)
+    }
+
+}
+
+class SongsListViewHolder(private val binding: SongViewBinding): RecyclerView.ViewHolder(binding.root){
+
+    fun songsBinding(song: SongItem, onClickedSong: (Int) -> Unit){
+        binding.tvSongName.text = song.trackName
+        binding.tvArtistName.text = song.artistName
+        binding.tvSongPrice.text = song.trackPrice.toString()
+        Picasso.get().load(song.artworkUrl60).into(binding.ivSongCover)
+        itemView.setOnClickListener {
+            song.trackId?.let(onClickedSong)
+        }
+    }
+
+}
