80626ad503998f467ce4fae74b64b33b249e55bf
Sara Iza <sasita.iza@gmail.com>
2023-02-09 17:31:20 -0500

    Added menu using navigation Added Room DB

--- app/src/main/java/com/example/musicapp/model/domain/Song.kt
+++ app/src/main/java/com/example/musicapp/model/domain/Song.kt
@@ -1,15 +1,24 @@
 package com.example.musicapp.model.domain
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.musicapp.model.SongItem
 
+@Entity
 data class Song(
+    @PrimaryKey
     val trackId: Int,
+    @ColumnInfo("song_name")
     val trackName: String,
     val artistName: String,
+    @ColumnInfo("image_url")
     val artworkUrl60: String,
+    @ColumnInfo("album_name")
     val collectionName: String,
     val previewUrl: String,
     val releaseDate: String,
+    @ColumnInfo("song_price")
     val trackPrice: Double
 )
 
